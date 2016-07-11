976887a7156609bf428a6b46f09a00c8ed7dadcd
davidlenze <davidlenze@yahoo.de>
2016-07-11 15:26:56 +0200

    Änderung an der add function

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,3 @@
-
-    
 # Hier alle erforderlichen Importe
 import sqlite3
 import os.path
@@ -55,15 +53,10 @@ def show_entries():
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into entries (text) values (?)',
-            [ request.form['text']])
-    #g.db.execute('insert into entries (room) values (?)',
-            #[ request.form['text']])
-    #g.db.execute('insert into entries (customer) values (?)',
-            #[ request.form['text']])
-    #g.db.execute('insert into entries (recordno) values (?)',
-            #[ request.form['integer']])
-    g.db.commit()
+    db = get_db()
+    db.execute('insert into entries date, room, customer, recordno () values (?, ?, ?, ?)',
+                 [request.form['date'], request.form['text']])
+    db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
